1d01a383542786a4ade0894e96d1ced4261b2120
Martín Buceta <mbuceta@grantaire.com.ar>
2013-11-18 19:18:12 -0300

    Add acts as can do it method to Module

--- lib/can_do_it.rb
+++ lib/can_do_it.rb
@@ -8,3 +8,15 @@ require "can_do_it/unknown"
 module CanDoIt
   # Your code goes here...
 end
+
+Module.class_eval do
+  def acts_as_can_do_it(options={})
+    can_do_it_module = case options[:as].to_s
+                            when 'known' then CanDoIt::Known
+                            when 'unknown' then CanDoIt::Unknown
+                            else CanDoIt::Base
+                       end
+    self.send(:include,  can_do_it_module)
+    define_method(:auto_can_do_it_method?){true} if options[:auto]
+  end
+end
--- spec/base_spec.rb
+++ spec/base_spec.rb
@@ -1,7 +1,7 @@
 describe CanDoIt::Base do
 
   before(:all) do
-    class CanDoItImp; include CanDoIt::Base; end
+    class CanDoItImp; acts_as_can_do_it; end
   end
 
   it "must respond to can_see?" do
@@ -43,5 +43,82 @@ describe CanDoIt::Base do
     can_do_it_impl = CanDoItImp.new
     expect{can_do_it_impl.can_create?(nil,nil)}.to raise_error(NotImplementedError)
   end
+
+  context "without auto can_do_it method generation" do
+
+    before(:all) do
+      class CanDoItImpWithoutAuto; acts_as_can_do_it(:auto => false); end
+    end
+
+    it "must respond to can_see?" do
+      can_do_it_impl = CanDoItImpWithoutAuto.new
+      can_do_it_impl.respond_to?(:can_see?).should  == true
+    end
+
+    it "must raise NotImplementedError when call can_see?" do
+      can_do_it_impl = CanDoItImpWithoutAuto.new
+      expect{can_do_it_impl.can_see?(nil)}.to raise_error(NotImplementedError)
+    end
+
+    it "mustn't respond to can_view?" do
+      can_do_it_impl = CanDoItImpWithoutAuto.new
+      can_do_it_impl.respond_to?(:can_view?).should  == false
+    end
+
+    it "must raise NoMethodError when call can_view?" do
+      can_do_it_impl = CanDoItImpWithoutAuto.new
+      expect{can_do_it_impl.can_view?(nil)}.to raise_error(NoMethodError)
+    end
+
+    it "must respond to can_eliminate?" do
+      can_do_it_impl = CanDoItImpWithoutAuto.new
+      can_do_it_impl.respond_to?(:can_eliminate?).should  == false
+    end
+
+    it "must raise NoMethodError when call can_eliminate?" do
+      can_do_it_impl = CanDoItImpWithoutAuto.new
+      expect{can_do_it_impl.can_eliminate?(nil)}.to raise_error(NoMethodError)
+    end
+
+  end
+
+  context "with auto can_do_it method generation" do
+    before(:all) do
+      class CanDoItImpWithAuto; acts_as_can_do_it(:auto => true); end
+    end
+
+    it "must respond to can_see?" do
+      can_do_it_impl = CanDoItImpWithAuto.new
+      can_do_it_impl.respond_to?(:can_see?).should  == true
+    end
+
+    it "must raise NotImplementedError when call can_see?" do
+      can_do_it_impl = CanDoItImpWithAuto.new
+      expect{can_do_it_impl.can_see?(nil)}.to raise_error(NotImplementedError)
+    end
+
+    it "must respond to can_view?" do
+      can_do_it_impl = CanDoItImpWithAuto.new
+      can_do_it_impl.respond_to?(:can_view?).should  == true
+    end
+
+    it "must raise NoMethodError when call can_view?" do
+      can_do_it_impl = CanDoItImpWithAuto.new
+      expect{can_do_it_impl.can_view?(nil)}.to raise_error(NotImplementedError)
+    end
+
+    it "must respond to can_eliminate?" do
+      can_do_it_impl = CanDoItImpWithAuto.new
+      can_do_it_impl.respond_to?(:can_eliminate?).should  == true
+    end
+
+    it "must raise NoMethodError when call can_eliminate?" do
+      can_do_it_impl = CanDoItImpWithAuto.new
+      expect{can_do_it_impl.can_eliminate?(nil)}.to raise_error(NotImplementedError)
+    end
+
+
+  end
+
 
 end
