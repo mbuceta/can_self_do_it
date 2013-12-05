615508e441003ccebaab8fe31715d5f7eced5f0f
Martín Buceta <mbuceta@grantaire.com.ar>
2013-12-05 18:36:27 -0300

    Use public_methods instead of respond_to?

--- lib/can_self_do_it/auto.rb
+++ lib/can_self_do_it/auto.rb
@@ -6,24 +6,19 @@ module Auto
   # Everything defined as /can_.+\?/ that does not exists get name from param type
   # i.e methods like can_do_something?(project) => can_do_something_project
   def method_missing(symbol, *args, &block)
-    if symbol.to_s == 'can_modify_default?'
-      raise NotImplementedError.new("You must implement can_modify_default?.")
-    elsif can_self_do_it_method?(symbol)
+    if can_self_do_it_method?(symbol)
       raise ArgumentError, "wrong number of arguments(#{args.size}1 for 1)"  unless args.size == 1
       obj = args[0]
-      new_symbol = "#{symbol.to_s[0..-2]}_#{CanSelfDoIt::Helper.class_2_method_sub_str(obj.class)}?"
-      respond_to_without_can_self_do_it_method?(new_symbol) ? send(new_symbol, obj) : can_modify_default?(obj)
+      new_symbol = "#{symbol.to_s[0..-2]}_#{CanSelfDoIt::Helper.class_2_method_sub_str(obj.class)}?".to_sym
+      self.public_methods.include?(new_symbol) ? send(new_symbol, obj) : can_modify_default?(obj)
     else
       super
     end
   end
 
-
-  def respond_to_with_can_self_do_it_method?(*args)
-    can_self_do_it_method?(*args) || respond_to_without_can_self_do_it_method?(*args)
+  def respond_to_missing?(symbol, include_all)
+    can_self_do_it_method?(symbol) || super
   end
-
-  alias_method :respond_to?, :respond_to_with_can_self_do_it_method?
 
   protected
 
--- lib/can_self_do_it/base.rb
+++ lib/can_self_do_it/base.rb
@@ -4,12 +4,12 @@ module Base
 
   def can_see?(obj)
     method = "can_see_#{CanSelfDoIt::Helper.class_2_method_sub_str(obj.class)}?"
-    respond_to_without_can_self_do_it_method?(method) ?  self.send(method,obj) : can_see_default?(obj)
+    self.public_methods.include?(method.to_sym) ?  self.send(method,obj) : can_see_default?(obj)
   end
 
   def can_edit?(obj)
     method = "can_edit_#{CanSelfDoIt::Helper.class_2_method_sub_str(obj.class)}?"
-    respond_to_without_can_self_do_it_method?(method) ?  send(method,obj) : can_edit_default?(obj)
+    self.public_methods.include?(method.to_sym) ? send(method,obj) : can_edit_default?(obj)
   end
 
   # parent: parent of the object created
@@ -18,15 +18,17 @@ module Base
   # - session_user.can_create?(Project)
   def can_create?(obj_class, parent = self)
     method = "can_create_#{CanSelfDoIt::Helper.class_2_method_sub_str(obj_class)}?"
-    respond_to_without_can_self_do_it_method?(method) ?  send(method,parent) : can_create_default?(parent)
+    self.public_methods.include?(method.to_sym) ?  send(method,parent) : can_create_default?(parent)
   end
 
   def can_delete?(obj)
     method = "can_delete_#{CanSelfDoIt::Helper.class_2_method_sub_str(obj.class)}?"
-    respond_to_without_can_self_do_it_method?(method) ?  send(method,obj) : can_delete_default?(obj)
+    self.public_methods.include?(method.to_sym) ?  send(method,obj) : can_delete_default?(obj)
   end
 
-  alias_method :respond_to_without_can_self_do_it_method?, :respond_to?
+  def respond_to_without_can_self_do_it_method?(sym)
+    self.public_methods.include?(sym.to_sym)
+  end
 
 protected
 
@@ -34,7 +36,6 @@ protected
   def can_edit_default?(obj);          raise NotImplementedError.new("You must implement can_edit_default?.")   ; end
   def can_create_default?(parent);     raise NotImplementedError.new("You must implement can_create_default?.") ; end
   def can_delete_default?(obj);        raise NotImplementedError.new("You must implement can_delete_default?.") ; end
-
 
 end
 end
